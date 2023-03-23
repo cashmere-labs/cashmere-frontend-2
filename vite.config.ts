794e925000d45669e1a80006216557ab60f36a05
bakatrouble <bakatrouble@gmail.com>
2023-03-23 14:53:38 +0300

    chunk splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,40 @@
 import svgx from '@svgx/vite-plugin-react';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 export default defineConfig(async () => {
     return {
         plugins: [
             react(),
             svgx({ defaultImport: 'url' }),
+            chunkSplitPlugin({
+                strategy: 'single-vendor',
+                customChunk: args => {
+                    const { file } = args;
+                    const chunks = ['VeCSM', 'DAO', 'Swap'];
+                    for (const chunk of chunks) {
+                        if (file.startsWith(`src/pages/${chunk}`) || file.startsWith(`src/components/${chunk}`)) {
+                            return chunk;
+                        }
+                    }
+                    return null;
+                },
+                customSplitting: {
+                    'react': [
+                        'react',
+                        'react-dom',
+                        'react-cool-dimensions',
+                        'react-modern-calendar-datepicker',
+                        'react-responsive',
+                        'react-router-dom',
+                        'react-slider',
+                        'react-toastify'
+                    ],
+                    'ethereum': ['ethers', 'wagmi', '@wagmi/core', '@rainbow-me/rainbowkit'],
+                    'state': ['mobx', 'mobx-react-lite', 'inversify', 'inversify-react'],
+                }
+            })
         ],
         build: {
             rollupOptions: {

990d29a2858688da7c5c2bdb5b85edf16a2745e8
bakatrouble <bakatrouble@gmail.com>
2023-03-23 14:17:52 +0300

    fix prod env detection

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -1,5 +1,5 @@
 export const NUMBER_REGEX = /^-?\d*\.?\d*$/;
 
 export const IS_PROD = !import.meta.env.DEV;
-export const apiAddress = IS_PROD ? 'http://localhost:3003' : '';
+export const apiAddress = !IS_PROD ? 'http://localhost:3003' : '';
 

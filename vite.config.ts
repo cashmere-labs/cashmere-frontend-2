5b628b0241c903b1794de07ba6f6281c00d11b0c
bakatrouble <bakatrouble@gmail.com>
2023-03-23 15:02:15 +0300

    disable chunk splitting

--- vite.config.ts
+++ vite.config.ts
@@ -8,33 +8,42 @@ export default defineConfig(async () => {
         plugins: [
             react(),
             svgx({ defaultImport: 'url' }),
-            chunkSplitPlugin({
-                strategy: 'single-vendor',
-                customChunk: args => {
-                    const { file } = args;
-                    const chunks = ['VeCSM', 'DAO', 'Swap'];
-                    for (const chunk of chunks) {
-                        if (file.startsWith(`src/pages/${chunk}`) || file.startsWith(`src/components/${chunk}`)) {
-                            return chunk;
-                        }
-                    }
-                    return null;
-                },
-                customSplitting: {
-                    'react': [
-                        'react',
-                        'react-dom',
-                        'react-cool-dimensions',
-                        'react-modern-calendar-datepicker',
-                        'react-responsive',
-                        'react-router-dom',
-                        'react-slider',
-                        'react-toastify'
-                    ],
-                    'ethereum': ['ethers', 'wagmi', '@wagmi/core', '@rainbow-me/rainbowkit'],
-                    'state': ['mobx', 'mobx-react-lite', 'inversify', 'inversify-react'],
-                }
-            })
+            // chunkSplitPlugin({
+            //     strategy: 'single-vendor',
+            //     customChunk: args => {
+            //         const { file } = args;
+            //         const chunks = ['VeCSM', 'DAO', 'Swap'];
+            //         for (const chunk of chunks) {
+            //             if (file.startsWith(`src/pages/${chunk}`) || file.startsWith(`src/components/${chunk}`)) {
+            //                 return chunk;
+            //             }
+            //         }
+            //         return null;
+            //     },
+            //     customSplitting: {
+            //         'react': [
+            //             'react',
+            //             'react-dom',
+            //             'react-cool-dimensions',
+            //             'react-modern-calendar-datepicker',
+            //             'react-responsive',
+            //             'react-router-dom',
+            //             'react-slider',
+            //             'react-toastify'
+            //         ],
+            //         'ethereum': ['ethers', 'wagmi', '@wagmi/core', '@rainbow-me/rainbowkit'],
+            //         'state': ['mobx', 'mobx-react-lite', 'inversify', 'inversify-react'],
+            //     }
+            // })
         ],
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        es: [ 'ethers' ],
+                    }
+                }
+            }
+        }
     };
 });

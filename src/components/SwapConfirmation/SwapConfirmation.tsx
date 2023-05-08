883f64c1b10523f277b0ad14e229e5c2bf55fb82
pavel.sh <shumkovpav@gmail.com>
2023-05-08 14:50:36 +0300

    raise gas limit for estimation

--- src/components/SwapConfirmation/SwapConfirmation.tsx
+++ src/components/SwapConfirmation/SwapConfirmation.tsx
@@ -163,7 +163,7 @@ const SwapConfirmation = observer(({
         gasPrice: Big((await provider!.getGasPrice()).toString()).mul('2').toFixed(0),
         to: resp.to,
         value: resp.value,
-        gasLimit: 8000000,
+        gasLimit: 20000000,
       };
 
       console.log("beforeEstimate", tx);
